259d0b668e49c459129a4a470390b1e3c8dd582d
Akis Papadopoulos <i.papadopoulos@gnomon.com.gr>
2014-05-27 22:15:36 +0300

    Initializing the project for the first time.

---
+++ src/test/java/me/akiss/dsb/units/GenericTestCase.java
@@ -0,0 +1,21 @@
+package me.akiss.dsb.units;
+
+import org.apache.log4j.Logger;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * A generic unit test case implementation.
+ * 
+ * @author Akis Papadopoulos, iakopap@gmail.com
+ */
+public class GenericTestCase {
+
+    // Logger
+    private static final Logger logger = Logger.getLogger(GenericTestCase.class);
+    
+    @Test
+    public void testGenericMethod() {
+        assertTrue(true);
+    }
+}
