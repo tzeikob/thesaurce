c2d15dd0e087d91d0a753e3b62d642a2504350c9
Akis Papadopoulos <i.papadopoulos@gnomon.com.gr>
2014-05-29 00:01:35 +0300

    Working on testing multiple and single connection sessions.

--- src/test/java/me/akiss/dsb/units/GenericTestCase.java
+++ src/test/java/me/akiss/dsb/units/GenericTestCase.java
@@ -1,5 +1,10 @@
 package me.akiss.dsb.units;
 
+import java.util.ArrayList;
+import java.util.List;
+import me.akiss.dsb.db.ConnectionManager;
+import me.akiss.dsb.db.ConnectionPool;
+import me.akiss.dsb.db.ConnectionSession;
 import org.apache.log4j.Logger;
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -14,8 +19,29 @@ public class GenericTestCase {
     // Logger
     private static final Logger logger = Logger.getLogger(GenericTestCase.class);
     
+    // Connection manager
+    private static ConnectionManager cm = new ConnectionPool("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/lf62_plain?useUnicode=true&characterEncoding=UTF-8&useFastDateParsing=false", "root", "root");
+    
     @Test
-    public void testGenericMethod() {
-        assertTrue(true);
+    public void testConnection() {
+        ConnectionSession cs = cm.getSession();
+        
+        assertNotNull(cs);
+    }
+    
+    @Test
+    public void testMultipleConnections() {
+        List<ConnectionSession> sessions = new ArrayList<ConnectionSession>();
+        
+        cm.setMinIdleConnections(10);
+        cm.setMaxActiveConnections(31);
+        
+        for (int i = 0; i < 30; i++) {
+            ConnectionSession session = cm.getSession();
+            
+            sessions.add(session);
+        }
+        
+        assertTrue(sessions.size() == 30);
     }
 }
