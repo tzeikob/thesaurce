92bf310680b6fd4303d1381a8d5ffb93c755dd11
Akis Papadopoulos <i.papadopoulos@gnomon.com.gr>
2014-05-29 20:26:05 +0300

    Working on minor commentings and refactorings.

--- src/main/java/me/akiss/dsb/db/ConnectionManager.java
+++ src/main/java/me/akiss/dsb/db/ConnectionManager.java
@@ -1,14 +1,17 @@
 package me.akiss.dsb.db;
 
 /**
- * A poolable connection manager.
+ * An abstract data source connection manager.
  *
  * @author Akis Papadopoulos, iakopap@gmail.com
  */
 public interface ConnectionManager {
 
     public void setMinIdleConnections(int minIdleConnections);
+
     public void setMaxActiveConnections(int maxActiveConnections);
+
     public ConnectionSession getSession();
+
     public void reset();
 }
