c4cca94e8a0ebd385faa9d8e40766f8c4f56441f
Akis Papadopoulos <i.papadopoulos@gnomon.com.gr>
2014-05-31 00:10:54 +0300

    Adding a generic data encryption engine to future implementations.

---
+++ secure-data-engine/src/main/java/me/akiss/sde/encryptors/DataEncryptor.java
@@ -0,0 +1,11 @@
+package me.akiss.sde.encryptors;
+
+/**
+ * A generic data encryptor interface.
+ * 
+ * @author Akis Papadopoulos, iakopap@gmail.com
+ */
+public interface DataEncryptor {
+    
+    public String encrypt(String data);
+}
