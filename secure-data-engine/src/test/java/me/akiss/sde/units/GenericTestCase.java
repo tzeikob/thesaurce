58a0e692b15a838d62fa439e4a5b67316f3c2ff0
Akis Papadopoulos <i.papadopoulos@gnomon.com.gr>
2014-05-31 15:24:49 +0300

    Adding a test case testing random salt message uniqueness.

--- secure-data-engine/src/test/java/me/akiss/sde/units/GenericTestCase.java
+++ secure-data-engine/src/test/java/me/akiss/sde/units/GenericTestCase.java
@@ -1,5 +1,9 @@
 package me.akiss.sde.units;
 
+import java.util.HashSet;
+import java.util.Set;
+import me.akiss.sde.salters.Salter;
+import me.akiss.sde.salters.SecureRandomSalter;
 import org.apache.log4j.Logger;
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -15,7 +19,15 @@ public class GenericTestCase {
     private static final Logger logger = Logger.getLogger(GenericTestCase.class);
     
     @Test
-    public void testGenericMethod() {
-        assertTrue(true);
+    public void testSpill() {
+        Salter salter = new SecureRandomSalter();
+        
+        Set<String> salts = new HashSet<>();
+        
+        for(int i = 0; i < 10000; i++) {
+            salts.add(salter.spill());
+        }
+        
+        assertTrue(salts.size() == 10000);
     }
 }
