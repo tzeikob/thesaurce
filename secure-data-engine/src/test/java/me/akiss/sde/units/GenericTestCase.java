352c151033937a52e8a4bd610c2f3f20f63192bd
Akis Papadopoulos <i.papadopoulos@gnomon.com.gr>
2014-05-31 15:56:47 +0300

    Adding a test case testing the MD5 data encryption method.

--- secure-data-engine/src/test/java/me/akiss/sde/units/GenericTestCase.java
+++ secure-data-engine/src/test/java/me/akiss/sde/units/GenericTestCase.java
@@ -2,6 +2,8 @@ package me.akiss.sde.units;
 
 import java.util.HashSet;
 import java.util.Set;
+import me.akiss.sde.encryptors.DataEncryptor;
+import me.akiss.sde.encryptors.MessageDigestHasher;
 import me.akiss.sde.salters.Salter;
 import me.akiss.sde.salters.SecureRandomGenerator;
 import org.apache.log4j.Logger;
@@ -10,24 +12,41 @@ import org.junit.Test;
 
 /**
  * A generic unit test case implementation.
- * 
+ *
  * @author Akis Papadopoulos, iakopap@gmail.com
  */
 public class GenericTestCase {
 
     // Logger
     private static final Logger logger = Logger.getLogger(GenericTestCase.class);
-    
+
     @Test
     public void testSpill() {
         Salter salter = new SecureRandomGenerator();
-        
+
         Set<String> salts = new HashSet<>();
-        
-        for(int i = 0; i < 10000; i++) {
+
+        for (int i = 0; i < 10000; i++) {
             salts.add(salter.spill());
         }
-        
+
         assertTrue(salts.size() == 10000);
+    }
+
+    @Test
+    public void testMD5EncryptionMethod() {
+        DataEncryptor de = new MessageDigestHasher();
+
+        String password = "password";
+
+        String encryptedPassword = de.encrypt(password);
+
+        logger.info("Encryption[MD5]: '" + password + "':'" + encryptedPassword + "'");
+
+        String password2 = "password";
+
+        String encryptedPassword2 = de.encrypt(password2);
+
+        assertTrue(encryptedPassword.equals(encryptedPassword2));
     }
 }
