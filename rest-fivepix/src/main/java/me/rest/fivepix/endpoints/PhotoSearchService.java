a96fec6f04e1cc0806c232986bf9b4803bdb5bfd
Akis Papadopoulos <iakopap@gmail.com>
2016-01-14 21:42:44 +0200

    Adding another rest library for endpoints provided by the 500px.

---
+++ rest-fivepix/src/main/java/me/rest/fivepix/endpoints/PhotoSearchService.java
@@ -0,0 +1,153 @@
+package me.rest.fivepix.endpoints;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.kevinsawicki.http.HttpRequest;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import me.rest.utils.impl.PhotoItemExtractor;
+import me.rest.utils.model.PhotoItem;
+import me.rest.utils.model.PhotoItemPage;
+
+/**
+ * A photo web service consumer implementation using the 500px search photo API
+ * fetching photo items regarding both textual and geospatial parameters.
+ *
+ * @author Akis papadopoulos
+ */
+public class PhotoSearchService implements PhotoItemExtractor {
+
+    // Service URL
+    private String serviceURL;
+
+    // Service consumer key
+    private String consumerKey;
+
+    // Number of items per page
+    protected int pageSize = 20;
+
+    // Option to shuffle the results
+    protected boolean shuffle = false;
+
+    /**
+     * A constructor initiating a 500px photo consumer given the service
+     * credentials.
+     *
+     * @param serviceURL the service URL.
+     * @param consumerKey the consumer key.
+     * @param pageSize the number of items per page.
+     * @param shuffle true to shuffle the items order otherwise false.
+     */
+    public PhotoSearchService(String serviceURL, String consumerKey, int pageSize, boolean shuffle) {
+        this.serviceURL = serviceURL;
+        this.consumerKey = consumerKey;
+        this.pageSize = pageSize;
+        this.shuffle = shuffle;
+    }
+
+    /**
+     * A method sending a search request given various query parameters.
+     *
+     * @param text the optional text to search about.
+     * @param latitude the optional latitude cords of the point of interest.
+     * @param longitude the optional longitude cords of the point of interest.
+     * @param radius the optional radius around the point of interest in km.
+     * @param page the number of result page.
+     * @return a JSON raw text of the response.
+     * @throws Exception throws unknown error exceptions.
+     */
+    public String search(String text, Double latitude, Double longitude, Double radius, int page) throws Exception {
+        String result = null;
+
+        // Adding the request parameters
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("consumer_key", consumerKey);
+
+        // Setting optional text
+        if (text != null) {
+            params.put("term", text);
+        }
+
+        // Setting optional geospatial
+        if (latitude != null && longitude != null) {
+            if (radius != null) {
+                params.put("geo", latitude + "," + longitude + "," + radius + "km");
+            } else {
+                params.put("geo", latitude + "," + longitude + ",1km");
+            }
+        }
+
+        params.put("image_size", "30,200");
+        params.put("sort", "_score");
+        params.put("rpp", String.valueOf(pageSize));
+        params.put("page", String.valueOf(page));
+
+        // Sending the request
+        result = HttpRequest.get(serviceURL, params, true).accept("application/json").body();
+
+        return result;
+    }
+
+    /**
+     * A method extracting a result page of photo items given the JSON response
+     * in raw text form.
+     *
+     * @param json the JSON response in plain text raw format.
+     * @return a result page containing the list of photo items.
+     * @throws Exception throws unknown error exceptions.
+     */
+    @Override
+    public PhotoItemPage extract(String json) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(json);
+
+        // Extracting page related information
+        int page = rootNode.path("current_page").asInt();
+        int pages = rootNode.path("total_pages").asInt();
+        int count = rootNode.path("total_items").asInt();
+
+        // Extracting the photo items
+        JsonNode photosNode = rootNode.path("photos");
+        Iterator<JsonNode> items = photosNode.elements();
+
+        List<PhotoItem> photos = new ArrayList<PhotoItem>();
+
+        while (items.hasNext()) {
+            JsonNode item = items.next();
+
+            PhotoItem photo = new PhotoItem();
+
+            photo.setId(item.path("id").asText());
+            photo.setTitle(item.path("name").asText());
+            photo.setHits(item.path("votes_count").asInt());
+
+            photo.setLatitude(item.path("latitude").asDouble());
+            photo.setLongitude(item.path("longitude").asDouble());
+
+            String imageURL = item.path("image_url").asText();
+            photo.setThumbnailUrl(imageURL);
+
+            photo.setPhotoUrl("https://500px.com" + item.path("url").asText());
+
+            JsonNode userNode = item.path("user");
+            photo.setOwner(userNode.path("fullname").asText());
+            photo.setProfileUrl("https://500px.com/" + userNode.path("username").asText());
+
+            photo.setProvider("500px");
+
+            photos.add(photo);
+        }
+
+        // Shuffling items randomly
+        if (shuffle) {
+            Collections.shuffle(photos, new Random());
+        }
+
+        return new PhotoItemPage(page, pages, count, photos);
+    }
+}
