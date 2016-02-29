bae4ac9881e94ebe5df310acfba394cc61891406
Akis Papadopoulos <iakopap@gmail.com>
2016-02-29 14:58:21 +0200

    Working on ameizon rest library in order to post buffered image objects to the S3 storage service.

--- rest-ameizon/src/main/java/me/rest/ameizon/endpoints/AmazonStorageService.java
+++ rest-ameizon/src/main/java/me/rest/ameizon/endpoints/AmazonStorageService.java
@@ -10,8 +10,12 @@ import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.InputStream;
+import javax.imageio.ImageIO;
 
 /**
  * An amazon AWS S3 storage service consumer to upload file objects into the
@@ -66,7 +70,11 @@ public class AmazonStorageService {
         // Setting up the object put request
         String key = folderPath + "/" + file.getName();
 
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.length());
+
         PutObjectRequest object = new PutObjectRequest(bucket, key, file);
+        object.withMetadata(metadata);
 
         // Setting public access to the object
         if (publicAccess) {
@@ -87,7 +95,7 @@ public class AmazonStorageService {
      * @param fileName the file name.
      * @param contentType the content type of the file.
      * @param publicAccess set public access to the object.
-     * @return the MD5 token of the file after posted.
+     * @return the MD5 string of the file after posted.
      * @throws Exception throws unknown errors.
      */
     public String put(InputStream inputStream, long contentLength, String fileName, String contentType, boolean publicAccess) throws Exception {
@@ -109,5 +117,32 @@ public class AmazonStorageService {
         PutObjectResult result = client.putObject(object);
 
         return result.getContentMd5();
+    }
+
+    /**
+     * A method posting a given image object to the storage service given the
+     * bytes and various metadata.
+     *
+     * @param image the buffered image to post.
+     * @param fileName the name of the file.
+     * @param publicAccess set public access to the object.
+     * @return the md5 string of the file after posted.
+     * @throws Exception Exception throws unknown errors.
+     */
+    public String put(BufferedImage image, String fileName, boolean publicAccess) throws Exception {
+        // Getting the bytes of the image to be used as input stream
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        ImageIO.write(image, "jpg", baos);
+
+        baos.flush();
+        baos.close();
+
+        byte[] bytes = baos.toByteArray();
+
+        // Sending the put object request
+        String md5 = put(new ByteArrayInputStream(bytes), bytes.length, fileName, "image/jpeg", publicAccess);
+
+        return md5;
     }
 }
