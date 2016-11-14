8e555aacb450323e01f7bb52f7e35396ede0037f
Akis Papadopoulos <iakopap@gmail.com>
2016-11-14 19:41:48 +0200

    Moving a test case method to ultimate photo consumer module.

--- ultimate-photo-consumer/src/test/java/com/tkb/the/upc/unit/GenericTest.java
+++ ultimate-photo-consumer/src/test/java/com/tkb/the/upc/unit/GenericTest.java
@@ -21,11 +21,13 @@ public class GenericTest {
 
     private static final Logger logger = Logger.getLogger(GenericTest.class);
 
-    private static final String apiKey = "1a6b6b1013dbb8c633c3135103c4ad78";
+    private static final String flickrApiKey = "1a6b6b1013dbb8c633c3135103c4ad78";
+    
+    private static final String pxConsumerKey = "d7NkZlpxGAO1VHlhsJGhC2V8qYT7JPkKpPh5w5IO";
 
     @Test
-    public void testPhotosSearchService() throws Exception {
-        FlickerPhotoSearchService endpoint = new FlickerPhotoSearchService(apiKey, 60, OrderMode.MOST_RATED);
+    public void testFlickrPhotosSearchService() throws Exception {
+        FlickerPhotoSearchService endpoint = new FlickerPhotoSearchService(flickrApiKey, 60, OrderMode.MOST_RATED);
 
         String result = endpoint.search("christo redentor rio", null, 1);
 
@@ -45,6 +47,50 @@ public class GenericTest {
             StringBuilder context = new StringBuilder();
 
             context.append("<!DOCTYPE html><html><head>Flickr Results</head><body><ol>");
+
+            for (PhotoItem photo : page.getPhotos()) {
+                context.append("<li>")
+                        .append("<a href=\"" + photo.getPhotoUrl() + "\" target=\"_blank\">")
+                        .append("<img src=\"" + photo.getThumbnailUrl() + "\">")
+                        .append("</a></li>");
+            }
+
+            context.append("</ol></body></html>");
+
+            writer.write(context.toString());
+        } catch (IOException exc) {
+            throw exc;
+        } finally {
+            if (writer != null) {
+                writer.flush();
+                writer.close();
+            }
+        }
+        // TMP
+    }
+
+    @Test
+    public void test500pxPhotoSearchService() throws Exception {
+        FivepixPhotoSearchService endpoint = new FivepixPhotoSearchService(pxConsumerKey, 60, OrderMode.MOST_RELEVANT);
+
+        String result = endpoint.search("sydney opera", null, 1);
+
+        PhotoItemPage page = endpoint.extract(result);
+
+        List<PhotoItem> photos = page.getPhotos();
+        
+        logger.info("P: " + result.substring(0, 80));
+
+        assertTrue(photos != null && !photos.isEmpty());
+
+        // TMP
+        BufferedWriter writer = null;
+
+        try {
+            writer = new BufferedWriter(new FileWriter("/tmp/500px-result.html"));
+            StringBuilder context = new StringBuilder();
+
+            context.append("<!DOCTYPE html><html><head>500px Results</head><body><ol>");
 
             for (PhotoItem photo : page.getPhotos()) {
                 context.append("<li>")
