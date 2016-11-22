82579e2cc2bfddfe261b480ff347ba0613d18b57
Akis Papadopoulos <iakopap@gmail.com>
2016-11-22 23:06:10 +0200

    Forcing test case to failed.

--- amazon-s3-uploader/src/test/java/com/tkb/the/asu/unit/GenericTest.java
+++ amazon-s3-uploader/src/test/java/com/tkb/the/asu/unit/GenericTest.java
@@ -5,7 +5,6 @@ import java.io.File;
 import org.junit.Test;
 import org.apache.log4j.Logger;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 /**
  * A generic test case.
@@ -17,19 +16,13 @@ public class GenericTest {
     private static final Logger logger = Logger.getLogger(GenericTest.class);
 
     @Test
-    public void testStorageService() {
-        try {
-            AmazonStorageService ass = new AmazonStorageService("bucket", "EU_WEST_1", "uploads", "accessKey", "secretKey");
-            
-            File file = new File("path/to/file");
-            
-            String md5 = ass.put(file, true);
-            
-            assertNotNull(md5);
-        } catch(Exception exc) {
-            logger.error(exc);
-            
-            assertTrue(true);
-        }
+    public void testStorageService() throws Exception {
+        AmazonStorageService ass = new AmazonStorageService("bucket", "EU_WEST_1", "uploads", "accessKey", "secretKey");
+
+        File file = new File("/path/to/file");
+
+        String md5 = ass.put(file, true);
+
+        assertNotNull(md5);
     }
 }
