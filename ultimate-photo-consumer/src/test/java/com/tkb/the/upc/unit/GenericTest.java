c47f62b327a21a074e3e05039dca8aa4e61740a0
Akis Papadopoulos <iakopap@gmail.com>
2016-11-22 22:48:27 +0200

    Removing old deprecated service api keys for testing purposes.

--- ultimate-photo-consumer/src/test/java/com/tkb/the/upc/unit/GenericTest.java
+++ ultimate-photo-consumer/src/test/java/com/tkb/the/upc/unit/GenericTest.java
@@ -1,8 +1,5 @@
 package com.tkb.the.upc.unit;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.List;
 import com.tkb.the.upc.service.FlickerPhotoSearchService;
 import com.tkb.the.upc.model.OrderMode;
@@ -22,9 +19,9 @@ public class GenericTest {
 
     private static final Logger logger = Logger.getLogger(GenericTest.class);
 
-    private static final String flickrApiKey = "1a6b6b1013dbb8c633c3135103c4ad78";
+    private static final String flickrApiKey = "PASTE_FLICKR_API_KEY";
     
-    private static final String pxConsumerKey = "d7NkZlpxGAO1VHlhsJGhC2V8qYT7JPkKpPh5w5IO";
+    private static final String pxConsumerKey = "PASTE_500PX_CONSUMER_KEY";
 
     @Test
     public void testFlickrPhotosSearchService() throws Exception {
@@ -35,39 +32,8 @@ public class GenericTest {
         PhotoItemPage page = endpoint.extract(result);
 
         List<PhotoItem> photos = page.getPhotos();
-        
-        logger.info("P: " + result.substring(0, 80));
 
-        assertTrue(photos != null && !photos.isEmpty());
-
-        // TMP
-        BufferedWriter writer = null;
-
-        try {
-            writer = new BufferedWriter(new FileWriter("/tmp/flickr-result.html"));
-            StringBuilder context = new StringBuilder();
-
-            context.append("<!DOCTYPE html><html><head>Flickr Results</head><body><ol>");
-
-            for (PhotoItem photo : page.getPhotos()) {
-                context.append("<li>")
-                        .append("<a href=\"" + photo.getPhotoUrl() + "\" target=\"_blank\">")
-                        .append("<img src=\"" + photo.getThumbnailUrl() + "\">")
-                        .append("</a></li>");
-            }
-
-            context.append("</ol></body></html>");
-
-            writer.write(context.toString());
-        } catch (IOException exc) {
-            throw exc;
-        } finally {
-            if (writer != null) {
-                writer.flush();
-                writer.close();
-            }
-        }
-        // TMP
+        assertTrue("Please set a valid Flickr API KEY before testing.", photos != null && !photos.isEmpty());
     }
 
     @Test
@@ -79,38 +45,7 @@ public class GenericTest {
         PhotoItemPage page = endpoint.extract(result);
 
         List<PhotoItem> photos = page.getPhotos();
-        
-        logger.info("P: " + result.substring(0, 80));
 
-        assertTrue(photos != null && !photos.isEmpty());
-
-        // TMP
-        BufferedWriter writer = null;
-
-        try {
-            writer = new BufferedWriter(new FileWriter("/tmp/500px-result.html"));
-            StringBuilder context = new StringBuilder();
-
-            context.append("<!DOCTYPE html><html><head>500px Results</head><body><ol>");
-
-            for (PhotoItem photo : page.getPhotos()) {
-                context.append("<li>")
-                        .append("<a href=\"" + photo.getPhotoUrl() + "\" target=\"_blank\">")
-                        .append("<img src=\"" + photo.getThumbnailUrl() + "\">")
-                        .append("</a></li>");
-            }
-
-            context.append("</ol></body></html>");
-
-            writer.write(context.toString());
-        } catch (IOException exc) {
-            throw exc;
-        } finally {
-            if (writer != null) {
-                writer.flush();
-                writer.close();
-            }
-        }
-        // TMP
+        assertTrue("\"Please set a valid 500px CONSUMER KEY before testing.\",", photos != null && !photos.isEmpty());
     }
 }
